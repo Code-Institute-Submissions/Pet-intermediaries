57bad965e5f36e5bb5d8a95d206d6bfd690c3e13
KartexTiarc <christoffer.nyh@hotmail.com>
2021-02-10 12:49:40 +0000

    Starting trying to change colors

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,9 +1,9 @@
-$("light-mode").onClick(function(){
+$("light-mode").click(function(){
     $("light-mode").removeClass("dark-background")
     .$addClass("light-background");
 });
 
-$("dark-mode").onClick(function(){
+$("dark-mode").click(function(){
     $("dark-mode").removeClass("light-background")
     .addClass("dark-background");
 });
--- assets/js/dogs.js
+++ assets/js/dogs.js
@@ -1,4 +1,24 @@
 
 $("#pug-button").click(function(){
-    $("#pug-info").toggle();
+    $("#pug-info").slideToggle();
+});
+
+$("#german-button").click(function(){
+    $("#german-info").slideToggle();
+});
+
+$("#dane-button").click(function(){
+    $("#dane-info").slideToggle();
+});
+
+$("#wolfhound-button").click(function(){
+    $("#wolfhound").slideToggle();
+});
+
+$("#terrier-button").click(function(){
+    $("#terrier-info").slideToggle();
+});
+
+$("#bernard-button").click(function(){
+    $("#bernard-info").slideToggle();
 });
