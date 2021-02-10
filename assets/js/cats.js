043fa3ba90a3a314a3845e1417759c75c75cebe9
KartexTiarc <christoffer.nyh@hotmail.com>
2021-02-10 13:50:53 +0000

    Toggle button for cats

--- assets/js/cats.js
+++ assets/js/cats.js
@@ -1,3 +1,23 @@
 $("#maine-button").click(function(){
     $("#maine-info").slideToggle("slow");
 });
+
+$("#ragdoll-button").click(function(){
+    $("#ragdoll-info").slideToggle("slow");
+});
+
+$("#birma-button").click(function(){
+    $("#birma-info").slideToggle("slow");
+});
+
+$("#siamese-button").click(function(){
+    $("#siamese-info").slideToggle("slow");
+});
+
+$("#bengal-button").click(function(){
+    $("#bengal-info").slideToggle("slow");
+});
+
+$("#persian-button").click(function(){
+    $("#persian-info").slideToggle("slow");
+});
