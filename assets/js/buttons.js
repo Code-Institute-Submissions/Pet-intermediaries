0f5da05f2dc294b7b7155285347c982b26e7afdf
KartexTiarc <christoffer.nyh@hotmail.com>
2021-02-10 15:49:29 +0000

    Index site js started

--- assets/js/buttons.js
+++
@@ -1,9 +0,0 @@
-$("light-mode").click(function(){
-    $("light-mode").removeClass("dark-background")
-    .$addClass("light-background");
-});
-
-$("dark-mode").click(function(){
-    $("dark-mode").removeClass("light-background")
-    .addClass("dark-background");
-});
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+$("#intro").mouseenter(function(){
+    $("#intro").fadeOut(1000);
+});
+
+$("#intro").mouseenter(function(){
+    $("#introP").fadeIn(1500);
+});
