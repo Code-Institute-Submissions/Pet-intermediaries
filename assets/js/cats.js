13dbfb4ba31960d3a0bd51c8c34c6d4b7f4e654e
KartexTiarc <christoffer.nyh@hotmail.com>
2021-02-13 09:37:24 +0000

    Created const cats and dogs

--- assets/js/cats.js
+++ assets/js/cats.js
@@ -1,24 +1,25 @@
-$("#maine-button").click(function(){
-    $("#maine-info").slideToggle("slow");
-});
+const cats = [{
+        name: "Maine Coon",		
+        lifespan: "12 to 15 yers", 
+        colors: "Black Silver"},
+{
+    name: "Ragdoll", 
+    lifespan: "13 to 14 years", 
+    colors: "Black, Blue, White"},
+{
+	name: "Holy Birma",
+	lifespan: "15 years",
+	colors: "Blue, Chocolate, Red, Tortie" },
 
-$("#ragdoll-button").click(function(){
-    $("#ragdoll-info").slideToggle("slow");
-});
+{	name: "Siamese",
+	lifespan: "15 years",
+	colors: "Silver-gray, Brown, Blue"},
 
-$("#birma-button").click(function(){
-    $("#birma-info").slideToggle("slow");
-});
+{	name: "Bengal",
+	lifespan: "12 to 16 years",
+	colors: "Brown, Snow, Silver"},
 
-$("#siamese-button").click(function(){
-    $("#siamese-info").slideToggle("slow");
-});
-
-$("#bengal-button").click(function(){
-    $("#bengal-info").slideToggle("slow");
-});
-
-$("#persian-button").click(function(){
-    $("#persian-info").slideToggle("slow");
-});
+{	name: "Persian",
+	lifespan: "10 to 17 years",
+	colors: "Silver, White, Grey"}]
 
--- assets/js/dogs.js
+++ assets/js/dogs.js
@@ -1,12 +1,28 @@
 const dogs = [{
-        name: "pug",
+        name: "pug",		
         lifespan: "12 to 15 yers", 
         colors: "Black Silver"},
 {
     name: "Great Dane", 
     lifespan: "8 to 10 years", 
-    colors: "Black Blue"}]
+    colors: "Black Blue"},
+{
+	name: "German Sheperd",
+	lifespan: "9 to 13 years",
+	colors: "Black, Sobel, Black and silver" },
 
-function dogsBreeds() {
+{	name: "Irish wolfhound",
+	lifespan: "6 to 10 years",
+	colors: "Black, white, fawn, gray"},
+
+{	name: "Bostonterrier",
+	lifespan: "13 to 15 years",
+	colors: "Black and white, Brindle and white"},
+
+{	name: "Saint Bernard",
+	lifespan: "8 to 10 years",
+	colors: "Red and white, Brownish-yellow, Reddish-brown"}]
+
+    function dogsBreeds() {
     
 }
