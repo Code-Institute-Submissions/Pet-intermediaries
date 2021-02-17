ccec816d25f5a1cae20550b609a8a17366c8f95c
KartexTiarc <christoffer.nyh@hotmail.com>
2021-02-17 12:55:31 +0000

    Array removed

--- assets/js/dogs.js
+++ assets/js/dogs.js
@@ -22,36 +22,6 @@ $("#bernard-button").click(function(){
     $("#bernard-info").slideToggle("slow");
 });
 
-const dogs = [{
-        name: "pug",        
-        lifespan: "12 to 15 yers", 
-        colors: "Black Silver"},
-{
-    name: "Great Dane", 
-    lifespan: "8 to 10 years", 
-    colors: "Black Blue"},
-{
-    name: "German Sheperd",
-    lifespan: "9 to 13 years",
-    colors: "Black, Sobel, Black and silver" },
-
-{   name: "Irish wolfhound",
-    lifespan: "6 to 10 years",
-    colors: "Black, white, fawn, gray"},
-
-{   name: "Bostonterrier",
-    lifespan: "13 to 15 years",
-    colors: "Black and white, Brindle and white"},
-
-{   name: "Saint Bernard",
-    lifespan: "8 to 10 years",
-    colors: "Red and white, Brownish-yellow, Reddish-brown"}]
-
-
-   
-
-    
-
 function voice (){
    
        alert ("Woff");
